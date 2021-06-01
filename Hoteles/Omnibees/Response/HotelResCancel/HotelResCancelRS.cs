04af4645414d50994912f0d201b95106f8b3a0f2
Stiven Yepez <stiven.yepez.g@gmail.com>
2021-06-01 14:03:37 -0500

    Diagrama HotelResCancel

---
+++ Hoteles/Omnibees/Response/HotelResCancel/HotelResCancelRS.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hoteles.Omnibees.Response.HotelResCancel
+{
+    public class HotelResCancelRS
+    {
+        public Comment Comment { get; set; }
+
+        public CancelInfoRS CancelInfoRS { get; set; }
+    }
+
+    public class Comment
+    {
+        public string Description { get; set; }
+    }
+
+    public class CancelInfoRS
+    {
+        public CancelRules[] CancelRules { get; set; }
+    }
+
+    public class CancelRules
+    {
+        public float Amount { get; set; }
+        public int CurrencyCode { get; set; }
+    }
+}
